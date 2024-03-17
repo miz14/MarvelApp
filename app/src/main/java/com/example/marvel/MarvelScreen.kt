aaff724b2e95f20e593ac8a5af2cbad615412333
Vlad <vpashenko14@gmail.com>
2024-03-17 22:58:42 +0600

    Добавил прогрутку с тестовой картой героя

--- app/src/main/java/com/example/marvel/MarvelScreen.kt
+++ app/src/main/java/com/example/marvel/MarvelScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -25,7 +26,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.marvel.ui.HeroesRow
-import com.example.marvel.ui.components.HeroCard
 import com.example.marvel.ui.theme.MarvelTheme
 
 @Composable
@@ -47,6 +47,7 @@ fun MarvelApp() {
                     Text("Choose your hero",
                         style = MaterialTheme.typography.titleMedium
                     )
+                    Spacer(modifier = Modifier.height(40.dp))
                     HeroesRow()
                 }
             }
--- app/src/main/java/com/example/marvel/ui/HeroesRow.kt
+++ app/src/main/java/com/example/marvel/ui/HeroesRow.kt
@@ -1,24 +1,42 @@
 package com.example.marvel.ui
 
+import android.icu.text.IDNA.Info
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.snapping.SnapLayoutInfoProvider
 import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.marvel.R
+import com.example.marvel.ui.theme.MarvelTheme
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -27,23 +45,53 @@ fun HeroesRow() {
 
     val snappingLayout = remember(state) { SnapLayoutInfoProvider(state) }
     val flingBehavior = rememberSnapFlingBehavior(snappingLayout)
+
     LazyRow(
         modifier = Modifier
-            .fillMaxSize(),
-
+            .fillMaxWidth(),
         state = state,
         verticalAlignment = Alignment.CenterVertically,
         flingBehavior = flingBehavior
 
     ){
-        items(100) { index ->
-            Box(
+        items(10) { index ->
+            val painter = painterResource(R.drawable.test_image)
+            Card(
                 modifier = Modifier
-                    .size(300.dp)
-                    .padding(10.dp)
-                    .background(Color.Red)
+                    .fillParentMaxWidth()
+                    .padding(20.dp)
+                    .clip(RoundedCornerShape(18.dp))
             ) {
-                Text(text = index.toString())
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                ) {
+                    Image(
+                        painter = painter,
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .fillMaxSize()
+                    )
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .align(Alignment.BottomStart)
+
+
+                    ) {
+                        Text(
+                            text = "title",
+                            style = MaterialTheme.typography.titleLarge,
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text(
+                            text = "descr",
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
+                }
+
             }
         }
 
@@ -53,7 +101,9 @@ fun HeroesRow() {
 @Preview
 @Composable
 fun PreviewHeroesRow(){
-    HeroesRow()
+    MarvelTheme {
+        HeroesRow()
+    }
 }
 
 // https://i.pinimg.com/736x/6f/3a/a5/6f3aa5c8784e60563d787bceab7c8253.jpg
